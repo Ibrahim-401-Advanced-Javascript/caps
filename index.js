fb8f8b61f8c4dc0dd83532de4297edf14733fe3e
Cas Ibrahim <qasiraibrahim@gmail.com>
2020-09-01 17:13:12 -0700

    added separate folders for caps, driver, and vendor applications. server running

--- index.js
+++ index.js
@@ -3,14 +3,6 @@
 // EE = Event Emitter
 const EE = require('events');
 
-// global event pool, shared by all modules
 const emitter = new EE();
 
-// because we export the pool of events,
-// any module that imports this file will get the same event pool
-// technically we are exporting one instance of events
-// singleton!
-
 module.exports = emitter;
-
-console.log('helloooooooooo');
